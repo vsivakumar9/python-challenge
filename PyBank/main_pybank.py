3becb4b716dab2d8aa32aaa6c7ed2c6c20ce0cc6
ShivaNJ <32874023+vsivakumar9@users.noreply.github.com>
2018-05-25 13:07:22 -0400

    adding pybank completed

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -6,17 +6,18 @@ import os
 
 csvpath = os.path.join('budget_data_1.csv')
 #file = 'budget_data_1.csv'
-sum=0.0
-sumrevchange = 0.0
+sum=0.00
+sumrevchange = 0.00
 cnt=0
 revchngcnt=0
-avgrevchange=0
+avgrevchange=0.00
 revchange=0
-maxrev_increase=0
-maxrev_decrease=0
+maxrev_increase=0.00
+maxrev_decrease=0.00
 currev=0
 prevrev=0
-newdict=dict()
+
+outfile="budget_out.txt"
 
 
 
@@ -28,22 +29,67 @@ with open(csvpath, newline='')  as budgetcsv:
     ## Perform counts and totals for each row in the dict.
     for row in csvreader:
         #print(row["Revenue"])
-        
-        currev=float(row["Revenue"])
-        sum = sum + currev
         cnt=cnt+1
+        currev=float(row["Revenue"])
+        currdate=row["Date"]
+        sum = sum + currev
+        
+        #calculate revenue change for each month and capture max, min values. 
         if cnt > 1: 
             revchange = currev - prevrev
-            sumrevchange = sumrevchange + 1
+            sumrevchange = sumrevchange + revchange
             revchngcnt =  revchngcnt + 1  
-        print(str(revchange))
+            #print(str(revchange))
+        if revchange >= 0:
+            if revchange > maxrev_increase :
+                maxrev_increase = revchange
+                maxrev_inc_date = currdate
+                
+        elif revchange < 0:
+            if revchange < maxrev_decrease:
+                maxrev_decrease = revchange
+                maxrev_dec_date = currdate
+                
+        #print("max" + str(maxrev_increase)) 
+        #print("min" + str(maxrev_decrease))  
         #store the current rev in to the hold area for use.
         prevrev=currev    
     
+    #calculate average of revenuechange
+    avgrevchange = sumrevchange / revchngcnt
     
+    print(" ")
     print("Financial Analysis")
-    print("------------------------------------")
+    print("-"*50)
     print("Total months  : " + str(cnt))
     print("Total Revenue : " + str(sum))
+    print("Average Revenue Change $" + str(avgrevchange))
+    print("Greatest Increase in Revenue: " + maxrev_inc_date + " ($" +
+              str(maxrev_increase) +")" )
+    print("Greatest Decrease in Revenue: " + maxrev_dec_date + " ($" +
+              str(maxrev_decrease) +")" )
+    print(" ")
     
-## Create a dict that also contains the change in revenue for each mnth.
+    with  open(outfile,'w')  as fhout:
+        print("Financial Analysis", file=fhout,end='\n')
+        print("----------------------------------------", file=fhout,end='\n')
+        print("Total months  : " + str(cnt), file=fhout,end='\n')
+        print("Total Revenue : " + str(sum), file=fhout,end='\n')
+        print("Average Revenue Change $" + str(avgrevchange),file=fhout,end='\n')
+        print("Greatest Increase in Revenue: " + maxrev_inc_date + " ($" +
+              str(maxrev_increase) +")", file=fhout,end='\n' )
+        print("Greatest Decrease in Revenue: " + maxrev_dec_date + " ($" +
+              str(maxrev_decrease) +")", file=fhout,end='\n' )
+        
+        #print("End of file ",file=fhout,end='\n')
+        
+        #fhout.write("Financial Analysis")
+        #fhout.write("------------------------------------")
+        #fhout.write("Total months  : " + str(cnt) + "\n")
+        #fhout.write("Total Revenue : " + str(sum))
+        #fhout.write("Average Revenue Change $" + str(avgrevchange))
+        #fhout.write("Greatest Increase in Revenue: " + maxrev_inc_date + " ($" +
+        #      str(maxrev_increase) +")")
+        #fhout.write("Greatest Decrease in Revenue: " + maxrev_dec_date + " ($" +
+        #      str(maxrev_decrease) +")" )
+        
