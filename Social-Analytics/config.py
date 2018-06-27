04c4cb6ebdfc10b527b770f1327cc8c13d3f758b
ShivaNJ <32874023+vsivakumar9@users.noreply.github.com>
2018-06-27 10:08:27 -0400

    social-analytics 6-27a commit

---
+++ Social-Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "MerM2ONWQ6jGImQsKR8WNOPsV"
+consumer_secret = "OFo3HKNAzIzazxFvmik9nPZymtrSokKtYU1J8ZXN7C2qETmN4K"
+access_token = "363073498-mFbppWrZBvnddHeBjCzYgO5WOjvyOINKG2UnaKw3"
+access_token_secret = "dS9VIxF1pyxLvdb4zyRVNpT1wa9sBzALAahYKtuQ1M6h9"
